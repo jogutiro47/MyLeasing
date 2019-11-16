0b690847522f08f4e120d54e52f1353d6c483837
jogutiro47 <jogutiro@gmail.com>
2019-11-15 19:54:12 -0500

    Prueba Seeder

---
+++ MyLeasing.Web/Data/Entities/Manager.cs
@@ -0,0 +1,12 @@
+﻿namespace MyLeasing.Web.Data.Entities
+{
+    public class Manager
+    {
+
+        public int Id { get; set; }
+
+        //Hereda los campos de la Tabla User
+        public User User { get; set; }
+
+    }
+}
